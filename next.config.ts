397b034f83fc137b21ea55082ba75a9267f4fbf4
y_eon <y_eon@yeon-MacBook.local>
2024-12-19 15:37:05 +0900

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // This replaces `next export`
+  reactStrictMode: true,
+  trailingSlash: true, // Recommended for GitHub Pages
 };
 
 export default nextConfig;
