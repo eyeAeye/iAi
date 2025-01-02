3db3501c3c2b32f7e90888240e9cb4333a2c827d
eyeAeye <156554261+eyeAeye@users.noreply.github.com>
2025-01-02 18:57:56 +0900

    Update next.config.ts for GitHub Actions workflow

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export', // This replaces `next export`
   reactStrictMode: true,
   trailingSlash: true, // Recommended for GitHub Pages
+  basePath: isGithubPages ? '/iAi' : '',
+  assetPrefix: isGithubPages ? '/iAi' : '',
 };
 
 export default nextConfig;
